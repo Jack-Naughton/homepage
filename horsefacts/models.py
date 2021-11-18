44a45c2621adcff99acac12da0259b529d755f37
Jack Naughton <jack@canoeintelligence.com>
2021-11-18 15:27:35 -0500

    add a __str__ property for horsefacts and define the table name

--- horsefacts/models.py
+++ horsefacts/models.py
@@ -4,3 +4,9 @@ from django.db import models
 # Create your models here.
 class HorseFact(models.Model):
     fact = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.fact
+
+    class Meta:
+        db_table = 'horsefacts'
--- horsefacts/views.py
+++ horsefacts/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from horsefacts.models import HorseFact
+from .models import HorseFact
 
 
 # Create your views here.
 def random(request):
     # This is known to be slow with lots of records, but horsefacts will only be a couple hundred
-    horsefact = HorseFact.objects.order_by('?')[0]
-    return HttpResponse(horsefact.fact)
+    horsefact = HorseFact.objects.order_by('?').first()
+    return HttpResponse(horsefact)
