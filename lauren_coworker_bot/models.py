3f9ab80bbf4646aa2650fc158f69dc80e03ebb4c
Jack Naughton <jack@canoeintelligence.com>
2021-12-07 22:34:14 -0500

    change coworker statement field to text because its so long

--- lauren_coworker_bot/models.py
+++ lauren_coworker_bot/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class CoworkerStatement(models.Model):
-    statement = models.CharField(max_length=255)
+    statement = models.TextField(max_length=65535)
 
     class Meta:
         db_table = 'coworker_statements'
