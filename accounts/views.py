90f259eeff7089d3de4a5fcc5c2a0e5b0da92353
= <=>
2022-03-25 14:13:16 -0400

    profile page requires login

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
@@ -10,6 +12,7 @@ class SignupView(CreateView):
     success_url = reverse_lazy('login')
 
 
+@method_decorator(login_required, name='dispatch')
 class ProfileView(DetailView):
     template_name = 'accounts/profile.html'
 
