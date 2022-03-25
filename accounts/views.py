261902f59eebc0502698dfd71c3a4210359c917d
= <=>
2022-03-25 13:00:34 -0400

    extend CreateView for full functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 
-class SignupView(FormView):
-    template_name = './registration/signup.html'
+class SignupView(CreateView):
+    template_name = 'registration/signup.html'
     form_class = UserCreationForm
+    success_url = reverse_lazy('login')
 
     pass
