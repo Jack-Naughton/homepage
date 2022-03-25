e85d6ec0a961a0db66ae16d66a98dc397876fb4d
= <=>
2022-03-25 15:17:14 -0400

    add email field to form signup

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(
+        help_text='Needed for password reset.'
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.utils.decorators import method_decorator
-from django.views.generic import DetailView
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
+
+from accounts.forms import SignupForm
 
 
 class SignupView(CreateView):
     template_name = 'registration/signup.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    form_class = SignupForm
+    success_url = 'profile'
 
 
 @method_decorator(login_required, name='dispatch')
