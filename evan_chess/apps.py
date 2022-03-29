efc975f77463bc97b58bb82ac8c03ae014aa5805
= <=>
2022-03-29 13:06:09 -0400

    startapp for evans chess game

---
+++ evan_chess/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EvanChessConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'evan_chess'
--- homepage/settings.py
+++ homepage/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'tailwind_theme.apps.TailwindThemeConfig',
     'song_game.apps.SongGameConfig',
+    'evan_chess.apps.EvanChessConfig',
 
     # 3rd party apps
     'crispy_forms',
