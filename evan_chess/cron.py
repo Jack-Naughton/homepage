363b44e14a9b14731ea6d756b0b18ce0ec103e31
= <=>
2022-03-29 13:55:09 -0400

    make model and function to save a chess answer

--- evan_chess/cron.py
+++ evan_chess/cron.py
@@ -1,2 +1,13 @@
+from urllib.request import urlopen
+import json
+
+from evan_chess.models import Answer
+
+CHESS_ANSWER_URL = 'https://d1vwq1uqg5c4bn.cloudfront.net/'
+
+
 def get_todays_answer():
+    string = urlopen('https://d1vwq1uqg5c4bn.cloudfront.net/').read().decode('utf-8')
+    decode = json.loads(string)
+    Answer.objects.update_or_create(json=decode)
     pass
--- evan_chess/models.py
+++ evan_chess/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Answer(models.Model):
+    # I could parse the object into columns
+    # ... or I could be lazyyyyy
+    json = models.JSONField()
