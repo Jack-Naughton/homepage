f4b1202bc9f2b6d97c2eaa6990a103e3e7d6ba65
Jack Naughton <jnaughton@smallhold.com>
2022-05-26 11:32:24 -0400

    and it begins

--- stolen_wiki_game/models.py
+++ stolen_wiki_game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    slug = models.CharField()
+    slug = models.CharField(max_length=255)
 
     def __str__(self):
         return self.slug
